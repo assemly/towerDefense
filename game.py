9e9025b8178241176757d56f2cc3dc5f6b835f6a
tim <tim@techwithtim.net>
2019-05-01 16:46:31 -0400

    added support towers

--- game.py
+++ game.py
@@ -4,22 +4,29 @@ from enemies.scorpion import Scorpion
 from enemies.club import Club
 from enemies.wizard import Wizard
 from towers.archerTower import ArcherTowerLong, ArcherTowerShort
+from towers.supportTower import DamageTower, RangeTower
 import time
 import random
+pygame.font.init()
+
+lives_img = pygame.image.load(os.path.join("game_assets","heart.png"))
+star_img = pygame.image.load(os.path.join("game_assets","star.png"))
 
 
 class Game:
     def __init__(self):
-        self.width = 1200
+        self.width = 1250
         self.height = 700
         self.win = pygame.display.set_mode((self.width, self.height))
         self.enemys = [Club()]
-        self.towers = [ArcherTowerLong(300,200), ArcherTowerLong(700,600), ArcherTowerShort(200,600)]
+        self.attack_towers = [ArcherTowerLong(300, 200),  ArcherTowerLong(700, 600), ArcherTowerShort(200, 600)]
+        self.support_towers = [DamageTower(400,200)]
         self.lives = 10
         self.money = 100
         self.bg = pygame.image.load(os.path.join("game_assets", "bg.png"))
         self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
         self.timer = time.time()
+        self.life_font = pygame.font.SysFont("comicsans", 70)
 
     def run(self):
         run = True
@@ -46,11 +53,21 @@ class Game:
 
             # delete all enemies off the screen
             for d in to_del:
+                self.lives -= 1
                 self.enemys.remove(d)
 
-            # loop through towers
-            for tw in self.towers:
+            # loop through attack towers
+            for tw in self.attack_towers:
                 tw.attack(self.enemys)
+
+            # loop through attack towers
+            for tw in self.support_towers:
+                tw.support(self.attack_towers)
+
+            # if you lose
+            if self.lives <= 0:
+                print("You Lose")
+                run = False
 
             self.draw()
 
@@ -59,15 +76,30 @@ class Game:
     def draw(self):
         self.win.blit(self.bg, (0,0))
 
-        # draw towers
-        for tw in self.towers:
+        # draw attack towers
+        for tw in self.attack_towers:
+            tw.draw(self.win)
+
+        # draw support towers
+        for tw in self.support_towers:
             tw.draw(self.win)
 
         # draw enemies
         for en in self.enemys:
             en.draw(self.win)
 
+        # draw lives
+        text = self.life_font.render(str(self.lives), 1, (255,255,255))
+        life = pygame.transform.scale(lives_img,(50,50))
+        start_x = self.width - life.get_width() - 10
+
+        self.win.blit(text, (start_x - text.get_width() - 10, 13))
+        self.win.blit(life, (start_x, 10))
+
         pygame.display.update()
+
+    def draw_menu(self):
+        pass
 
 g = Game()
 g.run()
