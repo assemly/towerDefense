43910477d8f9dba3dfe37bbeadf3d61324ae6f93
tim <tim@techwithtim.net>
2019-05-01 13:30:27 -0400

    added movement for enemies

--- game.py
+++ game.py
@@ -1,25 +1,45 @@
 import pygame
 import os
+from enemies.scorpion import Scorpion
+from enemies.club import Club
+from enemies.wizard import Wizard
 
 class Game:
     def __init__(self):
-        self.width = 1000
+        self.width = 1200
         self.height = 700
         self.win = pygame.display.set_mode((self.width, self.height))
-        self.enemys = []
+        self.enemys = [Wizard()]
         self.towers = []
         self.lives = 10
         self.money = 100
         self.bg = pygame.image.load(os.path.join("game_assets", "bg.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
 
     def run(self):
         run = True
         clock = pygame.time.Clock()
         while run:
-            clock.tick(60)
+            pygame.time.delay(500)
+            clock.tick(30)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+
+            # loop through enemies
+            to_del = []
+            for en in self.enemys:
+                if en.x < -5:
+                    to_del.append(en)
+
+            # delete all enemies off the screen
+            for d in to_del:
+                self.enemys.remove(d)
 
             self.draw()
 
@@ -27,4 +47,13 @@ class Game:
 
     def draw(self):
         self.win.blit(self.bg, (0,0))
+
+        # draw enemies
+
+        for en in self.enemys:
+            en.draw(self.win)
+
         pygame.display.update()
+
+g = Game()
+g.run()
